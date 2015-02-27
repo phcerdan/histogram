25e1c1590cca583c976adecacfcf51a097d0ef85
phcerdan <pablo.hernandez.cerdan@outlook.com>
2015-02-28 00:00:09 +1300

    Functional version of Histo. Todo: Add more BreaksMethods --Check R. Document BalanceBreaks functions and helpers.

---
+++ src/histo.h
@@ -0,0 +1,396 @@
+/*
+Histogram. Basic struct to handle histograms.
+Copyright (C) 2015 Pablo Hernandez
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HISTO_H_
+#define HISTO_H_
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <stdexcept>
+#include <cmath>
+#include <iostream>
+namespace histo {
+/**
+ * @brief Breaks method to optimal calculation of breaks based on input data and range.
+ * Scott = 0.
+ */
+enum breaks_method {Scott = 0};
+
+/**
+ * @brief Help functions to manually creating breaks from input range
+ * (low, upper) and desired number of bins.
+ *
+ * @tparam T Input data type.
+ * @tparam PRECI It should be equal to T, except when T is int.
+ * @param low first value of breaks.
+ * @param upper last value of breaks.
+ * @param bins number of divisions.
+ *
+ * @return breaks vector with the frontier values of each bin.
+ */
+template<typename T, typename PRECI = double>
+std::vector<T> GenerateBreaksFromRangeAndBins(const T& low, const T& upper, const unsigned long int &bins){
+    std::vector<T> breaks(bins+1);
+    T width = (upper - low)/ static_cast<PRECI>(bins);
+    for (unsigned long int i = 0; i!=bins+1; i++){
+        breaks[i] = low + i*width;
+    }
+    return breaks;
+};
+
+template<typename T, typename PRECI = double>
+std::vector<T> GenerateBreaksFromRangeAndBins(const std::pair<T,T> &range_low_upper, const unsigned long int &bins){
+   T low   = range_low_upper.first;
+   T upper = range_low_upper.second;
+   return GenerateBreaksFromRangeAndBins<T, PRECI>(low, upper, bins);
+};
+
+/**
+ * @brief Variance calculation from Container with data.
+ *
+ * @tparam T Type of data.
+ * @tparam Container std:: type containing data (vector, array,...)
+ * @param xs The container.
+ *
+ * @return Variance of type T.
+ */
+template <typename T, typename Container>
+T variance_welford(const Container& xs)
+{
+    unsigned long long N = 0;
+    T M = 0, S = 0, Mprev = 0;
+    for(auto x : xs) {
+        ++N;
+        Mprev = M;
+        M += (x - Mprev) / N;
+        S += (x - Mprev) * (x - M);
+    }
+    return S / (N-1);
+}
+
+/********PRECISE COMPARISONS TEMPLATE UTILITIES************/
+/**@addtogroup PreciseComparisson
+ * PreciseComparisson Template utilities
+ * @{ */
+/** Precise comparison: equal than.
+* @param v1 type T, variable 1 to compare
+* @param v2 type T, variable 2 to compare
+* @return bool
+*/
+template<typename T>
+bool isequalthan(const T& v1, const T& v2)
+{
+    return std::abs(v1-v2)<= std::numeric_limits<T>::epsilon();
+}
+/** @} */
+
+class histo_error : public std::runtime_error {
+public:
+    histo_error(const std::string & s) : std::runtime_error(s){ };
+
+};
+
+/** Simple Histogram. PRECI is the precission of stored non-int values. PRECI_INTEGER is the preci for integer values. T is the data type. PRECI should be the same type as T, except when T is int.
+ *
+*/
+/**
+ * @brief Histogram inspired by R.
+ * Simple, no dependancies, header-only.
+ * It accepts different data values and precission.
+ *
+ * @tparam T Input data type.
+ * @tparam PRECI It should be equal to T, except when T is int.
+ * @tparam PRECI_INTEGER int type for counts data member.
+ */
+template <typename T, typename PRECI = double, typename PRECI_INTEGER = unsigned long int>
+struct Histo {
+
+/************* DATA *****************/
+    /** Low and upper limit for breaks. */
+    std::pair<T,T> range;
+    /** Value of the breaks between bins, [low,...,upper].
+     *  size.breaks = size.counts + 1. */
+    std::vector<T> breaks;
+    /** breaks.size() */
+    unsigned long int bins{0};
+    /** int vector holding the counts for each breaks interval.*/
+    std::vector<PRECI_INTEGER> counts;
+
+/********** CONSTRUCTORS ************/
+    Histo() = default;
+
+    /**
+     * @brief Constructor that takes range as the min, and max values of data.
+     *
+     * @param data
+     * @param method Method to calculate breaks from @histo::breaks_method
+     */
+    Histo( const std::vector<T> &data, histo::breaks_method method = Scott )
+    {
+        auto range_ptr = std::minmax_element(data.begin(), data.end());
+        range     = std::make_pair(*range_ptr.first, *range_ptr.second);
+        breaks    = CalculateBreaks(data, range, method);
+        bins      = static_cast<decltype(bins)>( breaks.size() - 1);
+        ResetCounts();
+        FillCounts(data);
+    };
+
+    /**
+     * @brief Constructor with fixed input range.
+     * @param data
+     * @param input_range low and upper value
+     * @param method Method to calculate breaks from @histo::breaks_method
+     */
+    Histo(const std::vector<T> &data, const std::pair<T,T> &input_range, histo::breaks_method method = Scott ){
+        range     = input_range;
+        breaks    = CalculateBreaks(data, range, method);
+        bins      = static_cast<decltype(bins)>( breaks.size() - 1 );
+        ResetCounts();
+        // FillCounts(data);
+    };
+    /**
+     * @brief Constructor that accepts a vector of breaks.
+     * You can use @histo::GenerateBreaksFromRangeAndBins
+     * to help you creating the vector from specific number of bins and range.
+     *
+     * @param data
+     * @param input_breaks
+     */
+    Histo(const std::vector<T> &data, const std::vector<T> &input_breaks) {
+        breaks = input_breaks;
+        range  = std::make_pair(breaks[0], breaks[breaks.size() - 1]);
+        bins    = static_cast<decltype(bins)>( breaks.size() - 1 );
+        ResetCounts();
+        FillCounts(data);
+    };
+
+/********* PUBLIC METHODS ***********/
+    /**
+     * @brief Return the index of @counts associated to the input value
+     *
+     * @param value Ranging from range.first to range.second
+     *
+     * @return Index of counts
+     */
+    unsigned long int IndexFromValue(const T &value){
+        unsigned long int lo{0},hi{bins}, newb; // include right border in the last bin.
+        if(value >= breaks[lo] && (value < breaks[hi] || histo::isequalthan<T>(value,breaks[hi]) )){
+            while( hi - lo >= 2){
+                newb = (hi-lo)/2;
+                if (value >= breaks[newb]) lo = newb;
+                else hi = newb;
+            }
+        } else {
+            throw histo_error(" IndexFromValue: "+ std::to_string(value) +  " is out of bonds");
+        }
+
+        return lo;
+    };
+
+    void ResetCounts(){
+        counts.resize(bins);
+        std::for_each(std::begin(counts), std::end(counts),
+                [](PRECI_INTEGER & vh){ vh = 0; });
+    };
+
+    /**
+     * @brief Fill counts from data.
+     * Breaks must have been set-up before calling this method.
+     *
+     * @param data
+     *
+     * @return Reference to the data member @counts
+     */
+    std::vector<PRECI_INTEGER>& FillCounts(const std::vector<T> &data){
+        std::for_each(std::begin(data),std::end(data),
+                [this](const T & v){
+                    auto i   = IndexFromValue(v);
+                    counts[i]++;
+                });
+        return counts;
+    };
+
+    void Increase(const unsigned long int & index){
+        if (counts[index] == max_integer_)
+            throw histo_error("Increase has exceded PRECI_INTEGER."
+                    " Index: " + std::to_string(index) + " Value: " + std::to_string(counts[index]) );
+        counts[index]++;
+    };
+
+    void Decrease(const unsigned long int & index){
+        if (counts[index] <= 0 )
+            throw histo_error("Decrease has reached negative value."
+                    " Index: " + std::to_string(index) + " Value: " + std::to_string(counts[index]) );
+        counts[index]--;
+    };
+
+    void SetCount(const unsigned long int & index, const long double & v ){
+        if (v < 0 || v > max_integer_)
+            throw histo_error("Decrease has reached negative value."
+                    " Index: " + std::to_string(index) + " Value: " + std::to_string(counts[index]) );
+        counts[index] = static_cast<PRECI_INTEGER>(v);
+    };
+
+
+private:
+    /** Max integer for the current PRECI_INTEGER type*/
+    PRECI_INTEGER max_integer_{std::numeric_limits<PRECI_INTEGER>::max()};
+
+    bool CheckIfMonotonicallyIncreasing(const std::vector<T> &input_breaks){
+        T prev_value = input_breaks[0];
+        for( auto it = input_breaks.begin() + 1, it_end = input_breaks.end();
+                it!=it_end; it++)
+        {
+            if(*it <= prev_value) return false;
+            prev_value = *it;
+        }
+        return true;
+    }
+    /**
+     * @brief Method to wrap breaks calculation methods that
+     * take into account the input data and range to optimize breaks vector.
+     *
+     * @param data
+     * @param rang Range of breaks vector (low, upper)
+     * @param method Method to calculate breaks from @histo::breaks_method
+     *
+     * @return Reference to data member: breaks.
+     */
+    std::vector<T>& CalculateBreaks(const std::vector<T> & data, const std::pair<T,T> & rang, histo::breaks_method method ){
+        switch(method) {
+        case Scott:
+             return ScottMethod(data,rang);
+             break;
+        default:
+             throw histo_error("CalculateBreaks: No Valid Method selected to calculate breaks.");
+        }
+    };
+
+    bool CheckBreaksAreEquidistant(const std::vector<T> & input_breaks){
+        T diff = input_breaks[1] - input_breaks[0];
+        for( auto it = input_breaks.begin() + 1, it_end = input_breaks.end();
+                it!=it_end; it++)
+        {
+            if(!isequalthan<T>(*it - *(it-1), diff)) return false;
+        }
+        return true;
+    };
+
+    bool BalanceBreaksWithRange(std::vector<T> &input_breaks, std::pair<T,T> input_range){
+        if (!CheckBreaksAreEquidistant(input_breaks))
+            throw histo_error("BalanceBreaksWithRange cannot be applied in NON Equidistant breaks");
+
+        unsigned long int nbins = input_breaks.size() - 1;
+        T width                 = input_breaks[1] - input_breaks[0];
+        // diff_low is > 0 when it does not reach range, and < 0 when it goes beyond.
+        T diff_low              = input_breaks[0] - input_range.first;
+        // diff_upper is < 0 when it does not reach range, and >0 when it goes beyond.
+        T diff_upper            = input_breaks[nbins] - input_range.second;
+        bool diff_low_isZero   = isequalthan<T>(diff_low, 0);
+        bool diff_upper_isZero = isequalthan<T>(diff_upper, 0);
+
+        if (diff_low_isZero && diff_upper_isZero) return false;
+
+        // Put the first break in the range.first, and move all breaks accordingly.
+        if(!diff_low_isZero) ShiftBreaks(input_breaks, -diff_low);
+        CheckAndUpdateDiff(diff_upper, diff_upper_isZero, input_breaks[nbins], input_range.second);
+        if(diff_upper_isZero)  return true;
+
+        // Check if it is better to remove the last break than to add more.
+        // Biased to add more (1 is no bias).
+        double bias_to_add_bin = 0.8;
+        T diff_upper_before = input_breaks[nbins - 1] - input_range.second;
+        if ((diff_upper_before < 0) && (diff_upper > 0 )
+                && ( fabs(diff_upper_before) < bias_to_add_bin * fabs(diff_upper) ))
+        {
+            nbins--;
+            input_breaks.pop_back();
+            T width_to_expand = diff_upper_before/nbins;
+            ShrinkOrExpandBreaks(input_breaks, -width_to_expand);
+        }
+        CheckAndUpdateDiff(diff_upper, diff_upper_isZero, input_breaks[nbins], input_range.second);
+        if(diff_upper_isZero)  return true;
+
+        // If diff_upper is <0,  add bins until reach the upper range.
+        while(diff_upper < 0){
+            nbins++;
+            input_breaks.push_back(input_range.first + nbins * width);
+            diff_upper  = input_breaks[nbins] - input_range.second;
+        }
+        diff_upper_isZero = isequalthan<T>(diff_upper, 0);
+        if (diff_upper_isZero) return true;
+
+        // If diff_upper > 0.
+        // Shrink the width of the breaks, and calculate the new positions.
+        T width_to_shrink = diff_upper / nbins;
+        ShrinkOrExpandBreaks(input_breaks, - width_to_shrink);
+        return true;
+    };
+
+    void CheckAndUpdateDiff(T & diff, bool & diff_isZero, const T & rhs, const T & lhs){
+        diff = rhs - lhs;
+        diff_isZero = isequalthan<T>(diff, 0);
+    };
+    void ShiftBreaks(std::vector<T> &input_breaks, const T & d){
+        std::for_each(std::begin(input_breaks), std::end(input_breaks),
+                [&d]( T & v){
+                    v = v + d;
+                });
+    };
+    void ShrinkOrExpandBreaks(std::vector<T> &input_breaks, const T & d){
+        unsigned long int i{0} ;
+        std::for_each(std::begin(input_breaks), std::end(input_breaks),
+                [&i, &d]( T & v){
+                    v = v + i*d;
+                    i++;
+                });
+    };
+    /**
+     * @brief Scott Method to calculate optimal breaks.
+     * Calculate variance from @histo::variance_welford
+     *
+     * @param data
+     * @param rang Range of breaks vector (low, upper)
+     *
+     * @return Reference to data member: breaks.
+     */
+    std::vector<T>& ScottMethod(const std::vector<T> &data, const std::pair<T,T> &rang){
+        PRECI sigma = variance_welford<PRECI,std::vector<T>>(data);
+        PRECI width  = 3.5 * sqrt(sigma) / static_cast<PRECI>( data.size() );
+        bins    = std::ceil( (rang.second - rang.first) / width);
+        breaks.resize(bins + 1 );
+        for (unsigned long int i = 0; i!=bins + 1; i++){
+            breaks[i] = range.first + i*width;
+        }
+        // std::cout << "Non balanced breaks" << std::endl;
+        // std::cout<< "bins is: " << bins <<" width is: "<< width << " sigma is: " << sigma  <<std::endl;
+        // std::cout << "first break" << breaks[0] << " Second Break" << breaks[bins]  << std::endl;
+        // std::for_each(std::begin(breaks), std::end(breaks), [](const T& v) {std::cout<<v <<std::endl;});
+
+        BalanceBreaksWithRange(breaks, range);
+        bins = breaks.size() - 1;
+        // std::cout << "Balanced new breaks" << std::endl;
+        // std::cout<< "bins is: " << bins <<" width is: "<< breaks[1]-breaks[0]  <<std::endl;
+        // std::cout << "first break" << breaks[0] << " Second Break" << breaks[bins]  << std::endl;
+        // std::for_each(std::begin(breaks), std::end(breaks), [](const T& v) {std::cout<<v <<std::endl;});
+        return breaks;
+    };
+};
+
+} // End of namespace histo
+#endif
---
+++ src/test/test_histo.cpp
@@ -0,0 +1,118 @@
+#include "gmock/gmock.h"
+#include "histo.h"
+#include <memory>
+#include <iostream>
+#include <random>
+using namespace testing;
+using namespace std;
+using namespace histo;
+
+/**
+ * @brief Test the constructor with int data and input_breaks.
+ */
+struct HistoIntInputBreaks : public ::testing::Test{
+    static vector<int> data;
+    static Histo<int>   h;
+};
+vector<int> HistoIntInputBreaks::data{-2,-1,0,1,2};
+Histo<int> HistoIntInputBreaks::h(data, histo::GenerateBreaksFromRangeAndBins<int>(-2,2,2));
+
+TEST_F(HistoIntInputBreaks, MembersAreCorrect) {
+    EXPECT_EQ(2, (int)h.bins);
+    std::vector<int> gen_breaks{-2,0,2};
+    EXPECT_TRUE(gen_breaks == h.breaks) ;
+    EXPECT_TRUE(std::make_pair(-2,2) == h.range);
+    EXPECT_EQ(2, (int)h.counts[0]);
+    EXPECT_EQ(3, (int)h.counts[1]);
+}
+TEST_F(HistoIntInputBreaks, AddMoreDataToExistingHisto) {
+    vector<int> extra_data{-1,-1,2};
+    h.FillCounts(extra_data);
+    EXPECT_EQ(4, (int)h.counts[0]);
+    EXPECT_EQ(4, (int)h.counts[1]);
+}
+/**
+ * @brief You can modify counts manually, or with methods Increase, Decrease, SetCount.
+ * The latter provides security checks, throwing exepction if exceed limits (big, or less than zero).
+ */
+TEST_F(HistoIntInputBreaks, ModifyCountsManuallyOrMethods){
+    EXPECT_EQ(0, h.IndexFromValue(-2));
+    auto old_count = h.counts[0]++;
+    EXPECT_EQ(old_count + 1, h.counts[0] );
+    // Manipulating counts directly do not throw expections.
+    EXPECT_NO_THROW(h.counts[0] = -2);
+    EXPECT_NO_THROW(h.counts[0]++);
+    // Decrease throws error.
+    h.counts[0] = 0;
+    EXPECT_THROW(h.Decrease(0), histo_error);
+    // Increase throws errors.
+    h.counts[0] = numeric_limits<unsigned long int>::max();
+    EXPECT_THROW(h.Increase(0), histo_error) << h.counts[0];
+    // SetCount throws errors.
+    EXPECT_THROW(h.SetCount(0, -2), histo_error) ;
+    EXPECT_THROW(h.SetCount(0, numeric_limits<long double>::max()), histo_error) ;
+}
+/**
+ * @brief Test the constructor with just double data and Scott method to generate breaks.
+ */
+struct HistoDoubleScott : public ::testing::Test{
+    static vector<double> data;
+    static Histo<double>   h;
+};
+vector<double> HistoDoubleScott::data{1.0,3.0,5.0};
+Histo<double> HistoDoubleScott::h(data, breaks_method::Scott);
+
+TEST_F(HistoDoubleScott, MembersAreCorrect) {
+    EXPECT_FLOAT_EQ(1.0, h.range.first);
+    EXPECT_FLOAT_EQ(5.0, h.range.second);
+    EXPECT_EQ(2, h.bins);
+    EXPECT_EQ(2, (int)h.counts.size());
+    vector<double> expected_breaks{1.0, 3.0,5.0};
+    EXPECT_TRUE(expected_breaks == h.breaks);
+    EXPECT_EQ(1, h.counts[0]);
+    EXPECT_EQ(2, h.counts[1]);
+
+}
+TEST_F(HistoDoubleScott, IndexFromValueWorks) {
+    ASSERT_THROW( h.IndexFromValue(1.0 - numeric_limits<double>::epsilon()), histo_error);
+    EXPECT_EQ(0, h.IndexFromValue(1.0));
+    EXPECT_EQ(0, h.IndexFromValue(h.breaks[1] - 2*numeric_limits<double>::epsilon())) << h.breaks[1];
+    EXPECT_EQ(1, h.IndexFromValue(h.breaks[1])) << h.breaks[1];
+    EXPECT_EQ(1, h.IndexFromValue(h.breaks[2]));
+    EXPECT_EQ(1, h.IndexFromValue(h.breaks[2] + numeric_limits<double>::epsilon()) );
+    EXPECT_EQ(1, h.IndexFromValue(h.breaks[2] + 2*numeric_limits<double>::epsilon()) );
+    ASSERT_THROW(h.IndexFromValue(h.breaks[2] + 3*numeric_limits<double>::epsilon()), histo_error);
+    ASSERT_THROW(h.IndexFromValue(10), histo_error);
+}
+/**
+ * @brief Test the constructor with long double, input_range, and specific method to generate breaks.
+ */
+struct HistoLongDoubleInputRange : public ::testing::Test{
+    static vector<long double> data;
+    static Histo< long double, long double>   h;
+};
+vector< long double> HistoLongDoubleInputRange::data{-0.8,0.1,0.9};
+Histo< long double, long double> HistoLongDoubleInputRange::h(data,std::make_pair(-1.0, 1.0), breaks_method::Scott);
+
+TEST_F(HistoLongDoubleInputRange, BalanceBreaksWorks) {
+    std::vector<long double> balanced_breaks{-1.0,0.0,1.0};
+    EXPECT_TRUE(balanced_breaks == h.breaks);
+}
+
+/**
+ * @brief LotsOfData
+ */
+static default_random_engine generator;
+static uniform_real_distribution<double> cosined(-1.0,1.0);
+
+TEST(HistoLotsOfDataInputRange, BalanceBreaksWorks) {
+    unsigned long int ndata = 10000;
+    std::vector<long double> data(ndata);
+    for(auto &x : data){
+        x = cosined(generator);
+    }
+    std::pair<long double, long double> input_pair = std::make_pair(-1.0, 1.0);
+    Histo< long double, long double, unsigned long long> h(data, input_pair, breaks_method::Scott);
+    EXPECT_FLOAT_EQ(input_pair.first , h.breaks[0]);
+    EXPECT_FLOAT_EQ(input_pair.second , h.breaks[h.bins]);
+}
