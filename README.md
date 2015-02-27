1ab4f1a7385dcaca5a70bc2f4c4285087e114a66
phcerdan <pablo.hernandez.cerdan@outlook.com>
2015-02-28 00:25:18 +1300

    gmock turned off, updated readme, change license in file.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # histo-header
 Histogram in C++11, 1D, simple header-only, inspired by R, calculate and optimize breaks automatically. Accepts different precissions.
+# Downloading
+You can use the header as is, just downloading it, and including it in your project.
+It has a LGPL3.0 license, so you might be interested in compiling it.
 
 # Compile the library.
 git clone https://github.com/phcerdan/histo-header.git
 mkdir histo-header/build
 cd histo-header/build
-ccmake ../src
+cmake ../src
+
+If you want the shared library:
+cmake ../src -DBUILD_SHARED_LIBS=1
 
 The library will be generated in build/lib
 
--- src/histo.h
+++ src/histo.h
@@ -1,19 +1,19 @@
 /*
-Histogram. Basic struct to handle histograms.
+Histogram in C++11, 1D, simple header-only, inspired by R, calculate and optimize breaks automatically. Accepts different precissions.
 Copyright (C) 2015 Pablo Hernandez
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef HISTO_H_
